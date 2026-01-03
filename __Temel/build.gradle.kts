89931af45c34ca8da42f0024cf4fe1a96ebb50a6
kraptor123 <kraptor121@gmail.com>
2026-01-04 02:41:01 +0300

    temel

--- __Temel/build.gradle.kts
+++ __Temel/build.gradle.kts
@@ -4,7 +4,7 @@ version = 0
 cloudstream {
     authors     = listOf("kraptor")
     language    = "tr"
-    description = "Setfilmizle sitemizde, donma yaşamadan Türkçe dublaj ve altyazılı filmleri ile dizileri muhteşem 1080p full HD kalitesinde izleyebilirsiniz."
+    description = "filmdizi"
 
     /**
      * Status int as the following:
@@ -15,5 +15,5 @@ cloudstream {
     **/
     status  = 1 // will be 3 if unspecified
     tvTypes = listOf("Movie") //Movie, AnimeMovie, TvSeries, Cartoon, Anime, OVA, Torrent, Documentary, AsianDrama, Live, NSFW, Others, Music, AudioBook, CustomMedia, Audio, Podcast,
-    iconUrl = "https://www.google.com/s2/favicons?domain=www.setfilmizle.de&sz=%size%"
+    iconUrl = "https://www.google.com/s2/favicons?sz=64&domain=yandex.com.tr"
 }
