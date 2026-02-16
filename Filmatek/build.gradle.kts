df3f9f6cf385565f5dca840819949e7e2135878a
ByAyzen <189399597+ByAyzen@users.noreply.github.com>
2026-02-16 22:19:22 +0300

    Update build.gradle.kts

--- Filmatek/build.gradle.kts
+++ Filmatek/build.gradle.kts
@@ -1,11 +1,11 @@
 // ! Bu araç @ByAyzen tarafından | @cs-karma için yazılmıştır.
-version = 2
+version = 3
 
 cloudstream {
     authors     = listOf("ByAyzen")
     language    = "tr"
     description = "Sosyalizmin sinemasının veritabanı."
-    status  = 0
+    status  = 1
     tvTypes = listOf("Movie") //Movie, AnimeMovie, TvSeries, Cartoon, Anime, OVA, Torrent, Documentary, AsianDrama, Live, NSFW, Others, Music, AudioBook, CustomMedia, Audio, Podcast,
     iconUrl = "https://t3.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=https://filmatek.net/&size=128"
 }
