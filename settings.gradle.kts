0088b267f6dfe0fd5e6c1cfcf9651a7e056beb26
ByAyzen <189399597+ByAyzen@users.noreply.github.com>
2026-03-04 02:48:58 +0300

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,16 +1,14 @@
 rootProject.name = "CloudstreamPlugins"
 
-// This file sets what projects are included.
-// All new projects should get automatically included unless specified in the "disabled" variable.
-
-val disabled = listOf("__Temel", "ExampleProvider")
+// This file sets what projects are included. All new projects should get automatically included unless specified in "disabled" variable.
 
 val buildAll = providers.gradleProperty("buildAll").getOrElse("false").toBoolean()
 
-File(rootDir, ".").eachDir { dir ->
-    val buildFile = java.io.File(dir, "build.gradle.kts")
-    if (!disabled.contains(dir.name) && buildFile.exists()) {
+val disabled = listOf("__Temel", "ExampleProvider")
 
+File(rootDir, ".").eachDir { dir ->
+    val buildFile = File(dir, "build.gradle.kts")
+    if (!disabled.contains(dir.name) && buildFile.exists()) {
         val content = buildFile.readText()
         val hasStatusZero = content.contains(Regex("""status\s*=\s*0"""))
 
@@ -25,6 +23,7 @@ File(rootDir, ".").eachDir { dir ->
 fun File.eachDir(block: (File) -> Unit) {
     listFiles()?.filter { it.isDirectory }?.forEach { block(it) }
 }
+
 
 // To only include a single project, comment out the previous lines (except the first one), and include your plugin like so:
 // include("PluginName")
