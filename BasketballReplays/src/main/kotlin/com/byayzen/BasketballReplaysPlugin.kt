862e75eae2069b4275b603d90152a29c339458d6
ByAyzen <189399597+ByAyzen@users.noreply.github.com>
2026-03-04 02:18:59 +0300

    Update BasketballReplaysPlugin.kt

--- BasketballReplays/src/main/kotlin/com/byayzen/BasketballReplaysPlugin.kt
+++ BasketballReplays/src/main/kotlin/com/byayzen/BasketballReplaysPlugin.kt
@@ -5,15 +5,13 @@ import android.content.Context
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 import com.lagradost.cloudstream3.plugins.Plugin
 import com.lagradost.cloudstream3.extractors.FileMoon
-import com.lagradost.cloudstream3.extractors.Odnoklassniki
 import com.lagradost.cloudstream3.extractors.OkRuSSL
-import com.lagradost.cloudstream3.extractors.Okrulink
 
 @CloudstreamPlugin
 class BasketballReplaysPlugin : Plugin() {
     override fun load(context: Context) {
-        // Ana API
         registerMainAPI(BasketballReplays())
         registerExtractorAPI(FileMoon())
+        registerExtractorAPI(OkRuSSL())
     }
 }
