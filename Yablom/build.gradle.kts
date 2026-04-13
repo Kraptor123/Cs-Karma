06394a442e3b80b27c3cdd6f17fde17e1ef2bad2
ByAyzen <189399597+ByAyzen@users.noreply.github.com>
2026-04-13 07:16:02 +0300

    Description change

--- Yablom/build.gradle.kts
+++ Yablom/build.gradle.kts
@@ -4,7 +4,7 @@ version = 1
 cloudstream {
     authors     = listOf("ByAyzen")
     language    = "fr"
-    description = "Un Max de choix entre films et séries en french stream complet hd 1080p vf ou vostfr<! avec frenchstream c est simple rapide et sans inscription."
+    description = "French site."
     status  = 1 // will be 3 if unspecified
     tvTypes = listOf("Movie") //Movie, AnimeMovie, TvSeries, Cartoon, Anime, OVA, Torrent, Documentary, AsianDrama, Live, NSFW, Others, Music, AudioBook, CustomMedia, Audio, Podcast,
     iconUrl = "https://t2.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=https://yablom.com/euvcw7/home/yablom/&size=128"
