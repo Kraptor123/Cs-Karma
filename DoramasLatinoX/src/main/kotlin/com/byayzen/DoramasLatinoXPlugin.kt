4392226547e35cf0c5f676687e85e04a0baeab1b
ByAyzen <189399597+ByAyzen@users.noreply.github.com>
2026-08-01 19:15:46 +0300

    Fix for Doramaslatinox

--- DoramasLatinoX/src/main/kotlin/com/byayzen/DoramasLatinoXPlugin.kt
+++ DoramasLatinoX/src/main/kotlin/com/byayzen/DoramasLatinoXPlugin.kt
@@ -11,5 +11,6 @@ class DoramasLatinoXPlugin: Plugin() {
         registerMainAPI(DoramasLatinoX())
         registerExtractorAPI(DoramasLatinoXExtractor())
         registerExtractorAPI(OkRuSSL())
+        registerExtractorAPI(AbyssExtractor())
     }
 }
