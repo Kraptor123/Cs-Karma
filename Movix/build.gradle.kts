aef159fc29e943e6af8201a0fff5cc48baaaeaad
ByAyzen <189399597+ByAyzen@users.noreply.github.com>
2026-08-23 17:45:50 +0300

    Movix LiveTV

--- Movix/build.gradle.kts
+++ Movix/build.gradle.kts
@@ -7,5 +7,5 @@ cloudstream {
     description = "Explorez les films, séries, collections, alertes et recommandations Movix."
     status  = 1 // will be 3 if unspecified
     tvTypes = listOf("Movie", "TvSeries", "Anime") //Movie, AnimeMovie, TvSeries, Cartoon, Anime, OVA, Torrent, Documentary, AsianDrama, Live, NSFW, Others, Music, AudioBook, CustomMedia, Audio, Podcast,
-    iconUrl = "https://t1.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=https://movix.rodeo/&size=128"
+    iconUrl = "https://t1.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=https://movix.fun/&size=128"
 }
