b13a6e9e2f0bfde7136dfd217e1e55d5be450c7f
ByAyzen <189399597+ByAyzen@users.noreply.github.com>
2026-09-13 08:33:41 +0300

    F1Fullraces disabled.

--- F1FullRaces/build.gradle.kts
+++ F1FullRaces/build.gradle.kts
@@ -1,11 +1,11 @@
 // ! Bu araç @ByAyzen tarafından | @CS-Karma için yazılmıştır.
-version = 10
+version = 11
 
 cloudstream {
     authors     = listOf("ByAyzen")
     language    = "en"
     description = "F1FullRaces"
-    status  = 1 // will be 3 if unspecified
+    status  = 0 // will be 3 if unspecified
     tvTypes = listOf("Live") //Movie, AnimeMovie, TvSeries, Cartoon, Anime, OVA, Torrent, Documentary, AsianDrama, Live, NSFW, Others, Music, AudioBook, CustomMedia, Audio, Podcast,
     iconUrl = "https://i.imgur.com/CcDyv7Z.png"
 }
